6fa95fcecd1f2454c9946746c85feafc33114fc2
Taylor Brandstetter <deadbeef@deadbeef-macbookpro.roam.corp.google.com>
2017-09-18 09:15:20 +0200

    Adding skeleton UdpTransport object, constructible from JS.
    Doesn't do anything, but we can now build on top of it.

---
+++ third_party/WebKit/Source/modules/quic/UdpTransport.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2017 Google Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "modules/quic/UdpTransport.h"
+
+#include "bindings/core/v8/ExceptionState.h"
+#include "core/dom/ExecutionContext.h"
+
+namespace blink {
+
+UdpTransport* UdpTransport::Create(ExecutionContext* context) {
+  return new UdpTransport(context);
+}
+
+UdpTransport::UdpTransport(ExecutionContext* context)
+    : SuspendableObject(context) {}
+
+UdpTransport::~UdpTransport() {
+}
+
+String UdpTransport::address() const {
+  return "";
+}
+
+void UdpTransport::setDestination(const String& address,
+                                  ExceptionState&) {
+}
+
+void UdpTransport::ContextDestroyed(ExecutionContext*) {
+  // Destroy stuff.
+}
+
+void UdpTransport::Suspend() {
+  // Suspend/resume event queue if we have one.
+}
+
+void UdpTransport::Resume() {
+}
+
+bool UdpTransport::HasPendingActivity() const {
+  // Prevents us from being garbage collected. Probably good enough for the
+  // hackathon.
+  return true;
+}
+
+DEFINE_TRACE(UdpTransport) {
+  SuspendableObject::Trace(visitor);
+}
+
+}  // namespace blink
---
+++ third_party/WebKit/Source/modules/quic/UdpTransport.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2017 Google Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef UdpTransport_h
+#define UdpTransport_h
+
+#include "core/dom/SuspendableObject.h"
+#include "modules/ModulesExport.h"
+#include "platform/bindings/ActiveScriptWrappable.h"
+#include "platform/bindings/ScriptWrappable.h"
+#include "platform/heap/GarbageCollected.h"
+#include "platform/wtf/text/WTFString.h"
+
+namespace blink {
+
+class ExceptionState;
+
+class MODULES_EXPORT UdpTransport : public GarbageCollectedFinalized<UdpTransport>,
+  public ScriptWrappable,
+    public ActiveScriptWrappable<UdpTransport>,
+                                    public SuspendableObject {
+  DEFINE_WRAPPERTYPEINFO();
+  USING_GARBAGE_COLLECTED_MIXIN(UdpTransport);
+
+ public:
+  static UdpTransport* Create(ExecutionContext*);
+  ~UdpTransport() override;
+
+  String address() const;
+  void setDestination(const String& address,
+                     ExceptionState&);
+
+  // SuspendableObject functions.
+  void ContextDestroyed(ExecutionContext*) override;
+  void Suspend() override;
+  void Resume() override;
+
+  // ScriptWrappable functions.
+  bool HasPendingActivity() const final;
+
+  DECLARE_VIRTUAL_TRACE();
+
+ private:
+  explicit UdpTransport(ExecutionContext*);
+};
+
+}  // namespace blink
+
+#endif  // UdpTransport_h
